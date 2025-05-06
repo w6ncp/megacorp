741204c46ab41d866389c2f6dec598d5ec64fc49
w6ncp <w6ncpham@gmail.com>
2025-05-06 11:30:38 -0700

    L: updated scripts/scanner.sh to look for SSN

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,3 +1,6 @@
 printf "\n====== SCANNING FOR CREDIT CARD NUMBERS ======\n"
 grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
 echo "========= CREDIT CARD SCAN COMPLETE =========="
+printf "\n==== SCANNING FOR SOCIAL SECURITY NUMBERS ====\n"
+grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "======= SOCIAL SECURITY SCAN COMPLETE ========"
