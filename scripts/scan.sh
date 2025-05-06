37ae72094f44c7200445f1501ccbe0381561c3f5
w6ncp <w6ncpham@gmail.com>
2025-05-06 11:29:45 -0700

    K: updated scripts/scanner.sh to look for CC numbers

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,3 @@
-# TODO: write the script
+printf "\n====== SCANNING FOR CREDIT CARD NUMBERS ======\n"
+grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "========= CREDIT CARD SCAN COMPLETE =========="
